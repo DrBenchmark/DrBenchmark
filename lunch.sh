409e3b57664a5d5ed3130b607c2573611c28aadb
Labrak Yanis <yanis.labrak@univ-avignon.fr>
2024-02-22 22:23:42 +0100

    delete

--- lunch.sh
+++
@@ -1,1 +0,0 @@
-python download_datasets_locally.py && python download_models_locally.py && sbatch run_all_jean_zay.sh
--- recipes/deft2019/scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Apache 2.0
-
-python finetuning_bert_ner.py --config="../yaml/ner.yaml" --model="$1" --fewshot="$2"
