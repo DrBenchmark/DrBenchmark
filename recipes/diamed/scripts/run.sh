0cf0f714bdf28e76a2cb1f5fd7c253f55af608bf
Rouvier Mickael <mickael.rouvier@univ-avignon.fr>
2024-04-23 15:12:56 +0200

    desactivate fewshot argument

--- recipes/diamed/scripts/run.sh
+++ recipes/diamed/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Apache 2.0
 
-python task_1_finetuning_bert_cls.py --config="../yaml/cls.yaml" --model="$1" --fewshot="$2"
+python task_1_finetuning_bert_cls.py --config="../yaml/cls.yaml" --model="$1"
